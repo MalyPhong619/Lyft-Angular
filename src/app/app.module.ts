15c57b4e79870a4a6ea5eafb25478b86df2e3ed4
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-03-29 09:02:18 -0700

    build log in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DriverComponent } from './driver/driver.component';
 import { RiderComponent } from './rider/rider.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DriverComponent,
-    RiderComponent
+    RiderComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule
