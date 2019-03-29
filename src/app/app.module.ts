f808135a74afc17b5e028e4a9a8945a52bb9d707
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-03-29 09:01:32 -0700

    build driver and rider components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DriverComponent } from './driver/driver.component';
+import { RiderComponent } from './rider/rider.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DriverComponent,
+    RiderComponent
   ],
   imports: [
     BrowserModule
