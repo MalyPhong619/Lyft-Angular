584ade78342c1b1ebb41926f3b9249efc4502090
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-03-29 14:03:35 -0700

    ng build is failing - unable to display driver's first name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent {
   title = 'Lyft Clone';
 
   drivers : Driver[] = [
-    new Driver('Maly', 'Phong', 28, 'Female')
-    new Driver('Andrea', 'Nicole', 27, 'Female')
+    new Driver('Maly', 'Phong', 28, 'Female'),
+    new Driver('Andrea', 'Nicole', 27, 'Female'),
     new Driver('Sunny', 'Lakang', 34, 'Male')
   ]
-  
+
 }
