6c4adfe390ad05dba4f10e4087520ccf42d4334b
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-03-29 13:53:18 -0700

    hard code 3 driver objects in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Lyft Clone';
+
+  drivers : Driver[] = [
+    new Driver('Maly', 'Phong', 28, 'Female')
+    new Driver('Andrea', 'Nicole', 27, 'Female')
+    new Driver('Sunny', 'Lakang', 34, 'Male')
+  ]
+  
 }
