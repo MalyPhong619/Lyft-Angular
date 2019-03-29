d41a4d86fe9ea985c2e75fee1d8f04784c1f1056
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-03-29 10:58:22 -0700

    create a rider model with first/last name, age, location and riding boolean

---
+++ src/app/models/rider.model.ts
@@ -0,0 +1,9 @@
+export class Rider {
+  public riding: boolean = false;
+  (
+    public first_name: string,
+    public last_name: string,
+    public age: number,
+    public location: string;
+  )
+}
