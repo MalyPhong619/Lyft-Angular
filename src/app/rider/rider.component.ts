07396f68de207fb2720c6da5b04853ba35ca0a38
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-04-05 08:47:10 -0700

    change styleUrl in rider.component.ts - build working

--- src/app/rider/rider.component.ts
+++ src/app/rider/rider.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rider',
   templateUrl: './rider.component.html',
-  styleUrls: ['./rider.component.css']
+  styleUrls: ['./rider.component.scss']
 })
 export class RiderComponent implements OnInit {
 
