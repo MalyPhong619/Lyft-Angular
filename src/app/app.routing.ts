161a40ef0c50ca12c0e21b97a1dce2978fe66184
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-04-07 22:40:00 -0700

    route MainWelcomeCompnent to app.router.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,12 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainWelcomeComponent } from './main-welcome/main-welcome.component'
 
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: MainWelcomeComponent
+  },
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
